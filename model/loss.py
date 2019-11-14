c738932446286042f3c857a0bc2859fd39253d57
zhjohnchan <gdst_czh@163.com>
2019-11-14 21:59:00 +0800

    add BCEWithLogits loss

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,15 @@
+import torch.nn as nn
 import torch.nn.functional as F
 
 
 def nll_loss(output, target):
     return F.nll_loss(output, target)
+
+
+def multilabel_soft_margin_loss(output, target):
+    return F.multilabel_margin_loss(output, target)
+
+
+def BCEWithLogitsLoss(output, target):
+    critirion = nn.BCEWithLogitsLoss()
+    return critirion(output, target)
