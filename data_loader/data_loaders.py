be17a53e5b7a525e9ba6efa6b7cde6933ae3e07f
zhjohnchan <gdst_czh@163.com>
2019-11-20 13:48:07 +0800

    add kidnet dataset

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,20 +1,6 @@
 from torchvision import datasets, transforms
-from data_loader.datasets import AbdomenDataset
+from data_loader.datasets import AbdomenDataset, KidneyDataset
 from base import BaseDataLoader
-
-
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 
 class AbdomenDataLoader(BaseDataLoader):
@@ -30,3 +16,16 @@ class AbdomenDataLoader(BaseDataLoader):
         self.dataset = AbdomenDataset(self.data_dir, train=training, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
+
+class KidneyDataLoader(BaseDataLoader):
+    """
+    Kidney data loader
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            # transforms.Normalize((0.5,), (0.5,))
+        ])
+        self.data_dir = data_dir
+        self.dataset = KidneyDataset(self.data_dir, train=training, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
--- data_loader/datasets.py
+++ data_loader/datasets.py
@@ -28,12 +28,14 @@ class AbdomenDataset(Dataset):
             gt_org = sitk.ReadImage(os.path.join(self.split_dir, 'GT', gt_name), sitk.sitkInt8)
             gt_array = sitk.GetArrayFromImage(gt_org)  # (96, 256, 256)
             gts.append(gt_array)
-            label = np.zeros((gt_array.shape[0], 14))
+            label = np.zeros((gt_array.shape[0], 13))
             for idx in range(gt_array.shape[0]):
                 for cls in np.unique(gt_array[idx]):
-                    label[idx, cls] = 1
+                    if cls != 0:
+                        label[idx, cls-1] = 1
             labels.append(label)
         self.cts = np.vstack(cts).astype('float32')
+        self.cts = self.cts / self.cts.max()
         self.gts = np.vstack(gts)
         self.labels = np.vstack(labels).astype('float32')
 
@@ -44,5 +46,48 @@ class AbdomenDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         sample = (self.transform(self.cts[idx]), self.gts[idx], torch.tensor(self.labels[idx]))
+        return sample
 
+class KidneyDataset(Dataset):
+    def __init__(self, root_dir, train, transform=None):
+        self.transform = transform
+        if train:
+            self.split_dir = os.path.join(root_dir, 'train')
+        else:
+            self.split_dir = os.path.join(root_dir, 'val')
+
+        ct_list = os.listdir(os.path.join(self.split_dir, 'CT'))
+        gt_list = os.listdir(os.path.join(self.split_dir, 'GT'))
+        ct_list.sort(key=lambda x: int(x[3:7]))
+        gt_list.sort(key=lambda x: int(x[5:9]))
+
+        cts = []
+        gts = []
+        labels = []
+        for ct_name, gt_name in zip(ct_list, gt_list):
+            ct_org = sitk.ReadImage(os.path.join(self.split_dir, 'CT', ct_name), sitk.sitkInt16)
+
+            ct_array = sitk.GetArrayFromImage(ct_org)  #(96, 256, 256)
+            cts.append(ct_array)
+            gt_org = sitk.ReadImage(os.path.join(self.split_dir, 'GT', gt_name), sitk.sitkInt8)
+            gt_array = sitk.GetArrayFromImage(gt_org)  # (96, 256, 256)
+            gts.append(gt_array)
+            label = np.zeros((gt_array.shape[0], 2))
+            for idx in range(gt_array.shape[0]):
+                for cls in np.unique(gt_array[idx]):
+                    if cls != 0:
+                        label[idx, cls-1] = 1
+            labels.append(label)
+        self.cts = np.vstack(cts).astype('float32')
+        self.cts = self.cts / self.cts.max()
+        self.gts = np.vstack(gts)
+        self.labels = np.vstack(labels).astype('float32')
+
+    def __len__(self):
+        return len(self.cts)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        sample = (self.transform(self.cts[idx]), self.gts[idx], torch.tensor(self.labels[idx]))
         return sample
